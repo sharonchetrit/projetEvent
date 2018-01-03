98771e91b82e694265448cb11d1be418f3f9137c
Sharon Chetrit <sharon.chetrit@hotmail.fr>
2018-01-03 10:56:07 +0200

    add coco pods

---
+++ ProjetEvent/SlideMenu.h
@@ -0,0 +1,13 @@
+//
+//  SlideMenu.h
+//  ProjetEvent
+//
+//  Created by Sharon Chetrit on 03/01/2018.
+//  Copyright © 2018 Sharon Chetrit. All rights reserved.
+//
+
+#ifndef SlideMenu_h
+#define SlideMenu_h
+
+
+#endif /* SlideMenu_h */
---
+++ ProjetEvent/SlideMenuVC.swift
@@ -0,0 +1,9 @@
+//
+//  SlideMenuVC.swift
+//  ProjetEvent
+//
+//  Created by Sharon Chetrit on 03/01/2018.
+//  Copyright © 2018 Sharon Chetrit. All rights reserved.
+//
+
+import Foundation
