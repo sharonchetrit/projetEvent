55ddc168c91b9fecd6aa09a56cf62aeaa305217b
Lulu <orone.l@hotmail.fr>
2018-01-07 10:18:24 +0200

    implement Profile

---
+++ ProjetEvent/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  ProjetEvent
+//
+//  Created by Mac on 04/01/2018.
+//  Copyright © 2018 Sharon Chetrit. All rights reserved.
+//
+
+import Foundation
+
+
+class User
+{
+    var name: String
+    var surname: String
+    
+}
+
