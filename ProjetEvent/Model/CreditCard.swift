c1c832f980e00ed6295d667a78df31d98ca55a01
David Mamou <david_mamou@live.fr>
2018-01-22 12:18:27 +0200

    repare the error

--- ProjetEvent/Model/CreditCard.swift
+++ ProjetEvent/Model/CreditCard.swift
@@ -7,31 +7,32 @@
 //
 
 import Foundation
-class Event: NSObject, NSCoding
+class CreditCard: NSObject, NSCoding
 {
     
     var name : String
-    var depart : String
-    var arrive : String
-    var eventDescription : String
+    var numeroCard : String
+    var cvv : String
+    var expiration : String
     
     static let DocumentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("events")
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("creditCards")
     
-    init(name:String, depart:String, arrive:String,eventDescription: String) {
+    init(name:String, numeroCard:String, cvv:String,expiration: String) {
         self.name = name
-        self.depart = depart
-        self.arrive = arrive
-        self.eventDescription = eventDescription
+        self.numeroCard = numeroCard
+        self.cvv = cvv
+        self.expiration = expiration
     }
     
     init(dictionary: [String : Any] )
     {
         self.name = (dictionary["name"] as? String)!
-        self.depart = (dictionary["depart"] as? String)!
-        self.arrive = (dictionary["arrive"] as? String)!
-        self.eventDescription = (dictionary["eventDescription"] as? String)!
+        self.numeroCard = (dictionary["numeroCard"] as? String)!
+        self.cvv = (dictionary["cvv"] as? String)!
+        self.expiration = (dictionary["expiration"] as? String)!
     }
+    
     
     
     func serialize() -> Dictionary<String,Any>
@@ -39,70 +40,74 @@ class Event: NSObject, NSCoding
         var dict : Dictionary<String,Any> = Dictionary()
         
         dict["name"] = self.name
-        dict["depart"] = self.depart
-        dict["arrive"] = self.arrive
-        dict["eventDescription"] = self.eventDescription
+        dict["numeroCard"] = self.numeroCard
+        dict["cvv"] = self.cvv
+        dict["expiration"] = self.expiration
         
         return dict
     }
     
-    static func loadFromFile() -> Array<Event>?
+    static func loadFromFile() -> Array<CreditCard>?
     {
-        return NSKeyedUnarchiver.unarchiveObject(withFile: Event.ArchiveURL.path) as? [Event]
+        return NSKeyedUnarchiver.unarchiveObject(withFile: CreditCard.ArchiveURL.path) as? [CreditCard]
     }
     
-    static func loadSampleFromPlist() -> [Event]
+    static func loadSampleFromPlist() -> [CreditCard]
     {
-        var sampleEvent : [Event] = []
+        var sampleCreditCard : [CreditCard] = []
         
-        var eventArray : [Any] = []
+        var creditCardArray : [Any] = []
         
-        if let path = Bundle.main.path(forResource: "events", ofType: "plist")
+        if let path = Bundle.main.path(forResource: "creditCards", ofType: "plist")
         {
-            eventArray = NSArray(contentsOf: URL(fileURLWithPath: path)) as! [Any] // we get the array from the file here!!!!!!!
+            if let url : URL = URL(fileURLWithPath: path)
+            {
+                creditCardArray = NSArray(contentsOf: url ) as! [Any] // we get the array from the file here!!!!!!!
+            }
+            
         }
         
-        if let array : [Dictionary<String,Any>] = eventArray as? [Dictionary<String, Any>] {
+        if let array : [Dictionary<String,Any>] = creditCardArray as! [Dictionary<String, Any>] {
             // Use your dict here
             
             for dict in array
             {
-                let newEvent = Event(dictionary: dict)
-                sampleEvent.append(newEvent)
+                let newCard = CreditCard(dictionary: dict)
+                sampleCreditCard.append(newCard)
             }
         }
         
-        return sampleEvent
+        return sampleCreditCard
     }
     
     
     
-    static func loadEventSample() -> Array<Event>
+    static func loadCreditCardSample() -> Array<CreditCard>
     {
-        let sampleEvents = [
-            Event(name: "Anniversaire", depart: "14-11-2018", arrive: "17-11-1998", eventDescription: "restaurant"),
+        let sampleCreditCard = [
+            CreditCard(name: "David", numeroCard: "1983298429473645", cvv: "654", expiration: "19-11-2019")
             ]
         
-        return sampleEvents
+        return sampleCreditCard
         
     }
     
-    static func saveToFile(events:Array<Event> )
+    static func saveToFile(creditCards:Array<CreditCard> )
     {
-        NSKeyedArchiver.archiveRootObject(events, toFile: Event.ArchiveURL.path)
+        NSKeyedArchiver.archiveRootObject(creditCards, toFile: CreditCard.ArchiveURL.path)
         
     }
     
-    static func openFromUserDefaults() -> [Event]?
+    static func openFromUserDefaults() -> [CreditCard]?
     {
-        var sampleEvents : [Event] = []
+        var sampleCreditCards : [CreditCard] = []
         
-        if let eventDicts : [Dictionary<String,Any>] = UserDefaults.standard.object(forKey: "Event") as? [Dictionary<String, Any>]
+        if let creditCardsDicts : [Dictionary<String,Any>] = UserDefaults.standard.object(forKey: "CreditCard") as? [Dictionary<String, Any>]
         {
-            for dict in eventDicts
+            for dict in creditCardsDicts
             {
-                let event : Event = Event(dictionary: dict)
-                sampleEvents.append(event)
+                let card : CreditCard = CreditCard(dictionary: dict)
+                sampleCreditCards.append(card)
             }
         }
         else
@@ -110,45 +115,42 @@ class Event: NSObject, NSCoding
             return nil
         }
         
-        return sampleEvents
+        return sampleCreditCards
         
     }
     
-    static func saveOnUserDefaults(events: [Event] )
+    static func saveOnUserDefaults(creditCards: [CreditCard] )
     {
         // serialize
         
-        var eventDicts : [Dictionary<String,Any>] = []
+        var creditCardDicts : [Dictionary<String,Any>] = []
         
-        for event : Event in events
+        for creditCard : CreditCard in creditCards
         {
-            eventDicts.append(event.serialize())
+            creditCardDicts.append(creditCard.serialize())
         }
         
-        UserDefaults.standard.set(eventDicts, forKey: "Event")
+        UserDefaults.standard.set(creditCardDicts, forKey: "CreditCard")
     }
     
     required convenience init?(coder aDecoder: NSCoder)
     {
         guard let name = aDecoder.decodeObject(forKey: "name") as? String,
-        let depart = aDecoder.decodeObject(forKey: "depart") as? String,
-        let arrive = aDecoder.decodeObject(forKey: "arrive") as? String,
-        let eventDescription = aDecoder.decodeObject(forKey: "eventDescription") as? String
+            let numeroCard = aDecoder.decodeObject(forKey: "numeroCard") as? String,
+            let cvv = aDecoder.decodeObject(forKey: "cvv") as? String,
+            let expiration = aDecoder.decodeObject(forKey: "expiration") as? String
             else {
                 return nil
         }
         
-        self.init(name: name, depart: depart, arrive: arrive, eventDescription: eventDescription)
+        self.init(name: name, numeroCard: numeroCard, cvv: cvv, expiration: expiration)
     }
     func encode(with aCoder: NSCoder) {
         
         aCoder.encode(name, forKey: "name")
-            aCoder.encode(depart, forKey: "depart")
-            aCoder.encode(arrive, forKey: "arrive")
-            aCoder.encode(eventDescription, forKey: "eventDescription")
+        aCoder.encode(numeroCard, forKey: "numeroCard")
+        aCoder.encode(cvv, forKey: "cvv")
+        aCoder.encode(expiration, forKey: "expiration")
     }
     
 }
-
-    
-
